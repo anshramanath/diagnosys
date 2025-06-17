de9ffe6e5cb5148fcc4186b7f9fe952eecd5b151
Ansh Ramanath <anshramanath04@gmail.com>
2025-06-17 10:56:36 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,9 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-      <html lang="en">
-        <body style={{ margin: 0, padding: 0, fontFamily: "sans-serif" }}>{children}</body>
-      </html>
-    )
-  }  
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, padding: 0, fontFamily: "sans-serif" }}>{children}</body>
+    </html>
+  )
+}  
